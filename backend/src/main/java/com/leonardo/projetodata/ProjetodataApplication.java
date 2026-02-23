c1949e7118fa3096b70fa9e1320eb2ee94e9297a
leofss <leofernandess11@gmail.com>
2026-02-23 16:58:17 -0300

    Initial commit - booting up project

---
+++ backend/src/main/java/com/leonardo/projetodata/ProjetodataApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.projetodata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetodataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetodataApplication.class, args);
+	}
+
+}
